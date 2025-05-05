3fe2b7b45b06e9e19e2563ff3c4761e812a1b7fe
Tibrahi <ibrahimtuyizere2@gmail.com>
2025-05-05 18:10:52 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: './', // This is important for GitHub Pages or Vercel
 })
